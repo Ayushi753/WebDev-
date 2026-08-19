7a8077a35ba8298073cbbd502a0464efec0d6b06
Ayushi753 <pandeyayushi586@gmail.com>
2026-08-19 15:03:27 +0530

    New Code

---
+++ Java_Script/Basic.js
@@ -0,0 +1,30 @@
+let num = 7;
+
+if (num % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+ 
+
+
+
+
+//WAP For identifying gender
+function identify(name, gender)
+{
+    if(gender === "male")
+    {
+        console.log(name + " Hello Mr");
+    }
+    else if(gender === "female")
+    {
+        console.log(name + " Hello Mdmji");
+    }
+    else 
+    {
+    console.log(name + " Hello");
+    }
+}
+// Function ko call kro bro
+identify("Aarya","female");
